3b1f9842905210f7fbf0123570a705cefeae810f
Artem <konevskiy-artem@mail.ru>
2023-11-06 17:11:49 +0700

    Adding the second method to tasks 1 and 2

--- Task 1 bitwise operations/second.c
+++ Task 1 bitwise operations/second.c
@@ -4,7 +4,13 @@ int main(){
     int a;
     printf("Enter a number: ");
     scanf("%d", &a);
-  
+
+    printf("The first way:  ");
+    for(int i = 31; i >=0; i--)
+        printf("%d", (a >> i) & 1);
+    
+
+    printf("\nThe second way: ");
     for(int i = 0; i < 32; i++){
         printf("%d", ((a >> 31) & 1)); 
         a = a << 1;
