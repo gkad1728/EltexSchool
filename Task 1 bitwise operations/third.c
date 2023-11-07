7b70f3821b47cc45e2fd126997ce95f613f85785
Artem <konevskiy-artem@mail.ru>
2023-11-07 08:16:01 +0700

    Adding the second method to the 3rd task. Combining screenshots of the output into a single file.

--- Task 1 bitwise operations/third.c
+++ Task 1 bitwise operations/third.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    unsigned int a, b = 0;
+    unsigned int a, b;
     printf("Enter a number: ");
     scanf("%d", &a);
-  
+
+    printf("The first way:  ");
+    b = 0;
+    for(int i = 31; i >= 0; i--)
+        b += (a >> i) & 1;
+    printf("%d", b);
+
+    printf("\nThe second way: ");
+    b = 0;
     for(int i = 0; i < 32; i++){ 
         if ((a >> 31) & 1)
             b++;
