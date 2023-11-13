0e283fb8108810dd08d6300200466bbbef26beac
Artem <konevskiy-artem@mail.ru>
2023-11-13 15:36:15 +0700

    complete fourth.c

--- Task 1 bitwise operations/fourth.c
+++ Task 1 bitwise operations/fourth.c
@@ -9,16 +9,17 @@ int main(){
     }
     c = a;
 
-    printf("Enter the correct number to replace previous one(1 or 0): ");
+    printf("Enter the correct number to replace previous one: ");
     scanf("%d", &b);
     
-    printf("The original number :   %u ", a);
+    printf("The original number :   %10u          ", a);
     for(int i = 0; i < 32; i++){
         printf("%u", ((a >> 31) & 1)); 
         a = a << 1;
     }
-    c = (c & ~(1 << 2)) | b << 2;
-    printf("\nModified number :       %d ", c);
+
+    c = (c & 0xFF00FFFF) | (b << 16); 
+    printf("\nModified number :       %10d          ", c);
     for(int i = 0; i < 32; i++){
         printf("%u", ((c >> 31) & 1)); 
         c = c << 1;
