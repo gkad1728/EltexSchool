dafc808abceb9158d7b05eeb26bb866835d2cd88
Artem <gkad1728@gmail.com>
2024-01-26 15:49:32 +0700

    add Task 7 Static lib

---
+++ Task 7 StLib/lib/div.c
@@ -0,0 +1,3 @@
+double div(int a, int b){
+    return a/b;
+}
---
+++ Task 7 StLib/main.h
@@ -0,0 +1,9 @@
+#ifndef __LIB_H__
+#define __LIB_H__
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+double div(int, int);
+
+#endif 
