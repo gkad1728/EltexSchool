c2acf0af431c0fb28013401a89fd4fea9ea82a36
Artem <gkad1728@gmail.com>
2024-06-04 19:58:25 +0700

    update rawsock

--- Task 16 socket/fourth/rawsock.c
+++ Task 16 socket/fourth/rawsock.c
@@ -1,37 +1,52 @@
-#include <stdio.h>
-#include <sys/un.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <string.h>
-#include <unistd.h> 
-#include <errno.h> 
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h> 
+#include "lib.h"
+
+struct massage{
+    short src_port;
+    short des_port;
+    short length;
+    short check_sum;
+    char payload[BUF_SIZE];
+};
 
 int main(){
-    int fd; 
-    char buf[15]; 
+    int fd;  
+    struct massage msg;
+    char buf[BUF_SIZE];
+    struct sockaddr_in server, client;
+    socklen_t size = sizeof(struct sockaddr_in);
 
     fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP); 
     
-#if(0)    
-    if(recvfrom(fd, buf, 15, 0, (struct sockaddr *) NULL, NULL) == -1){
-        printf("ERR recv\n");
-    }
-    else{
-        printf("RECV %s\n", buf);
-    }
-#endif
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(MAIN_PORT); 
 
-#if(1)
+    msg.src_port = htons(PORT);
+    msg.des_port = htons(MAIN_PORT);
+    msg.length = htons(BUF_SIZE);
+    msg.check_sum = 0;
+    strcat(msg.payload, "Hi");
+
+    if(sendto(fd, &msg, sizeof(msg), 0, (struct sockaddr *) &server, size) == -1){
+        printf("ERR send %d\n", errno);
+        close(fd);
+        return -1;
+    }
+
     while(1){
-        if(recvfrom(fd, buf, 15, 0, (struct sockaddr *) NULL, NULL) != -1){
-            printf("RECV %s\n", buf);
+        if(recvfrom(fd, buf, BUF_SIZE, 0, (struct sockaddr *) &server, &size) == -1){
+            printf("RECV %s\n", buf); 
+        }
+        printf("Что-то поймал");
+        if(server.sin_port == htons(MAIN_PORT)){
+            printf("- %s\n", buf);
             break;
         }
+        else{
+            printf(", но это не то, что нужно\n");
+        }
     }
-#endif
+
     close(fd);
     return 0;
 }
